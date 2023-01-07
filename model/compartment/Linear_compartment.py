151f1c164b71ccdefb1a4d0347593e979309b58e
hugwolf77 <hugwolf77@gmail.com>
2023-01-08 00:54:19 +0900

    230107_detop

---
+++ model/compartment/Linear_compartment.py
@@ -0,0 +1,15 @@
+
+import torch.nn as nn
+
+class Linear_compartment(nn.Module):
+    def __init__(self, input_dim, output_dim, dropout) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        
+        self.Linear = nn.Linear(input_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, input_x):
+        x1 = self.Linear(input_x)
+        return 
---
+++ model/compartment/Rnn_compartment.py
@@ -0,0 +1,14 @@
+
+import torch.nn as nn
+
+class Rnn_compartment(nn.Module):
+    def __init__(self, input_dim, hid_dim, n_layers, dropout) -> None:
+        super().__init__()
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        self.LSTM = nn.LSTM(input_dim, hid_dim, n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, input_x):
+        x1=self.dropout(input_x)
+        outputs, (hidden,cell) = self.LSTM(x1)
+        return outputs, hidden, cell
