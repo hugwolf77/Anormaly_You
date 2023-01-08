e535a6b464aa0cff81848df1c323afd20dfaf65e
hugwolf77 <hugwolf77@gmail.com>
2023-01-08 22:15:59 +0900

    230108_03_BLprint&baseStructure

--- workspace_01/model/equipment/Chain_Coupler.py
+++ workspace_01/model/equipment/Chain_Coupler.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 from compartment import compartment
-from data_provider.data_factory import data_provider
 
 class Coupler(nn.Module):
     def __init__(self, compartment : compartment, window_size, adj_seq) -> None:
@@ -13,4 +12,7 @@ class Coupler(nn.Module):
     def Adjust_seq(self):
         pass
     def Adjust_count_compartment(self):
+        pass
+    
+    def Set_DataOrder(self):
         pass
